e74f80dee46eeeda81f45db00970bef84bbe6730
ccuffari <75679158+ccuffari@users.noreply.github.com>
2026-09-17 14:28:24 +0200

    feat(dev): moduli sql_server/sql_database + wiring su RG rg-ia-dev-we-01 (KV esistente)

--- environments/dev/locals.tf
+++ environments/dev/locals.tf
@@ -1,15 +1,11 @@
 locals {
-  location = var.location
+  location            = "westeurope"
+  resource_group_name = "rg-ia-dev-we-01"
+  key_vault_name      = "link-kv-vnet"
+  sql_server_name     = "sql-ia-dev-we-01"
+  sql_database_name   = "sqldb-ia-dev-we-01"
   tags = {
     environment = "dev"
     managed_by  = "terraform"
   }
-
-  resource_group_name = "rg-ia-dev-we-01"
-  storage_account_name = "saiadevwe01"
-  key_vault_name       = "kviadevwe01"
-
-  data_factory_name = "adfiadevwe01"
-  sql_server_name   = "sqliadevwe01"
-  sql_database_name = "sqldbiadevwe01"
 }
--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,33 +1,26 @@
 data "azurerm_client_config" "current" {}
 
-module "resource_group" {
-  source   = "../../modules/resource_group"
-  name     = local.resource_group_name
-  location = local.location
-  tags     = local.tags
+data "azurerm_resource_group" "this" {
+  name = local.resource_group_name
 }
 
-module "storage_account" {
-  source              = "../../modules/storage_account"
-  name                = local.storage_account_name
-  resource_group_name = module.resource_group.name
-  location            = local.location
-  tags                = local.tags
-}
-
-module "key_vault" {
-  source              = "../../modules/key_vault"
+data "azurerm_key_vault" "this" {
   name                = local.key_vault_name
-  resource_group_name = module.resource_group.name
+  resource_group_name = data.azurerm_resource_group.this.name
+}
+
+module "sql_server" {
+  source              = "../../modules/sql_server"
+  name                = local.sql_server_name
+  resource_group_name = data.azurerm_resource_group.this.name
   location            = local.location
-  tenant_id           = data.azurerm_client_config.current.tenant_id
+  key_vault_id        = data.azurerm_key_vault.this.id
   tags                = local.tags
 }
 
-module "data_factory" {
-  source              = "../../modules/data_factory"
-  name                = local.data_factory_name
-  resource_group_name = module.resource_group.name
-  location            = local.location
-  tags                = local.tags
+module "sql_database" {
+  source    = "../../modules/sql_database"
+  name      = local.sql_database_name
+  server_id = module.sql_server.id
+  sku_name  = "GP_S_Gen5_1"
 }
