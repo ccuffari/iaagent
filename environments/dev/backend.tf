a3762595b76f8181faf6ff0d4ea7458c85548135
ccuffari <75679158+ccuffari@users.noreply.github.com>
2026-09-17 15:58:38 +0200

    feat(infra): modulo resource_group + ambiente dev con backend tfstate (rg-ai-dev-we-01)

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-we-aura-01"
-    storage_account_name = "storage"
-    container_name       = "<storage2>"
+    resource_group_name  = "rgtfstatedevwe01"
+    storage_account_name = "sttfstatedevwe01"
+    container_name       = "tfstate"
     key                  = "dev/terraform.tfstate"
     use_azuread_auth     = true
   }
--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,6 +1,6 @@
-module "rg_rg-ia-dev-we-01" {
+module "resource_group" {
   source   = "../../modules/resource_group"
-  name     = "rg-ia-dev-we-01"
+  name     = "rg-ai-dev-we-01"
   location = local.location
   tags     = local.tags
 }
