f3fc40a69991f151e6f5409fb5de9501a2f971de
ccuffari <75679158+ccuffari@users.noreply.github.com>
2026-09-17 14:34:00 +0200

    feat(dev): tutti i moduli (sa, kv, adf, sql, sqldb) + import risorse esistenti in rg-ia-dev-we-01

--- environments/dev/locals.tf
+++ environments/dev/locals.tf
@@ -1,9 +1,11 @@
 locals {
   location            = "westeurope"
   resource_group_name = "rg-ia-dev-we-01"
-  key_vault_name      = "link-kv-vnet"
-  sql_server_name     = "sql-ia-dev-we-01"
-  sql_database_name   = "sqldb-ia-dev-we-01"
+  storage_account_name = "<sa>"
+  key_vault_name       = "link-kv-vnet"
+  data_factory_name    = "<adf1>"
+  sql_server_name      = "sql-ia-dev-we-01"
+  sql_database_name    = "sqldb-ia-dev-we-01"
   tags = {
     environment = "dev"
     managed_by  = "terraform"
--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -4,9 +4,47 @@ data "azurerm_resource_group" "this" {
   name = local.resource_group_name
 }
 
-data "azurerm_key_vault" "this" {
+import {
+  to = module.storage_account.azurerm_storage_account.this
+  id = "/subscriptions/f4a32007-b8c2-4aee-9dc0-1421a27e36ad/resourceGroups/rg-ia-dev-we-01/providers/Microsoft.Storage/storageAccounts/<sa>"
+}
+
+module "storage_account" {
+  source                   = "../../modules/storage_account"
+  name                     = local.storage_account_name
+  resource_group_name      = data.azurerm_resource_group.this.name
+  location                 = local.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags                     = local.tags
+}
+
+import {
+  to = module.key_vault.azurerm_key_vault.this
+  id = "/subscriptions/f4a32007-b8c2-4aee-9dc0-1421a27e36ad/resourceGroups/rg-ia-dev-we-01/providers/Microsoft.KeyVault/vaults/link-kv-vnet"
+}
+
+module "key_vault" {
+  source              = "../../modules/key_vault"
   name                = local.key_vault_name
   resource_group_name = data.azurerm_resource_group.this.name
+  location            = local.location
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+  tags                = local.tags
+}
+
+import {
+  to = module.data_factory.azurerm_data_factory.this
+  id = "/subscriptions/f4a32007-b8c2-4aee-9dc0-1421a27e36ad/resourceGroups/rg-ia-dev-we-01/providers/Microsoft.DataFactory/factories/<adf1>"
+}
+
+module "data_factory" {
+  source              = "../../modules/data_factory"
+  name                = local.data_factory_name
+  resource_group_name = data.azurerm_resource_group.this.name
+  location            = local.location
+  tags                = local.tags
 }
 
 module "sql_server" {
@@ -14,7 +52,7 @@ module "sql_server" {
   name                = local.sql_server_name
   resource_group_name = data.azurerm_resource_group.this.name
   location            = local.location
-  key_vault_id        = data.azurerm_key_vault.this.id
+  key_vault_id        = module.key_vault.id
   tags                = local.tags
 }
 
