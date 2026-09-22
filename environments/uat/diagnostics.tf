d309291740356831bec02ef636b72751a4672de2
ccuffari <75679158+ccuffari@users.noreply.github.com>
2026-09-22 17:33:00 +0200

    feat(env): crea environments/uat (naming e state dedicati)

---
+++ environments/uat/diagnostics.tf
@@ -0,0 +1,3 @@
+# Diagnostic settings spostati nel modulo riutilizzabile modules/diagnostic_settings
+# e composti in environments/uat/main.tf (module.diag_*).
+# Questo file e' mantenuto vuoto per evitare duplicazione di risorse.
