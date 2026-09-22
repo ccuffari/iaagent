0b5e0265a58cdead433ec26fdbba3dae08f31f9e
ccuffari <75679158+ccuffari@users.noreply.github.com>
2026-09-22 17:48:44 +0200

    fix(prod): backend usa sttfstateprodwe02 (nome disponibile)

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rgtfstateprodwe01"
-    storage_account_name = "sttfstateprodwe01"
+    storage_account_name = "sttfstateprodwe02"
     container_name       = "tfstate"
     key                  = "prod/terraform.tfstate"
     use_azuread_auth     = true
