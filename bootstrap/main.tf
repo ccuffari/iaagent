6c5faf0ea47a24f0f7545ffebc0e82481ef6e4b4
ccuffari <75679158+ccuffari@users.noreply.github.com>
2026-09-22 21:15:27 +0200

    chore(bootstrap): rimuove moduli tfstate uat/prod (state unificato su dev)

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,11 +1,11 @@
-# Bootstrap dei backend di state Terraform per uat e prod.
+# Bootstrap dei backend di state Terraform.
 #
-# Questo ambiente e' SEPARATO dagli ambienti applicativi: crea i resource group
-# e gli storage account che ospitano lo state remoto di uat/prod. Va applicato
-# UNA TANTUM, prima di poter eseguire 'terraform init' su quegli ambienti.
+# NOTA: lo state di uat e prod e' stato UNIFICATO sullo storage di dev
+# (sttfstatedevwe01, container 'tfstate', key 'uat/' e 'prod/').
+# Non servono piu' storage di state dedicati per uat/prod.
 #
-# Lo state di questo bootstrap e' locale (non puo' stare su un backend che
-# ancora non esiste).
+# Questo ambiente resta come riferimento storico; i moduli tfstate_uat e
+# tfstate_prod sono stati rimossi perche' non piu' necessari.
 
 locals {
   location = "westeurope"
@@ -13,22 +13,4 @@ locals {
     purpose    = "terraform-state"
     managed_by = "terraform"
   }
-}
-
-module "tfstate_uat" {
-  source              = "../modules/tfstate_backend"
-  name                = "sttfstateuatwe01"
-  resource_group_name = "rgtfstateuatwe01"
-  location            = local.location
-  container_name      = "tfstate"
-  tags                = local.tags
-}
-
-module "tfstate_prod" {
-  source              = "../modules/tfstate_backend"
-  name                = "sttfstateprodwe01"
-  resource_group_name = "rgtfstateprodwe01"
-  location            = local.location
-  container_name      = "tfstate"
-  tags                = local.tags
 }
