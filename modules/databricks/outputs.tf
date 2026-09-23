2db5c2b44feab6edda94ccbb575fd6296ad5ed8f
ccuffari <75679158+ccuffari@users.noreply.github.com>
2026-09-23 12:59:59 +0200

    fix(databricks): rimuovi output principal_id (identity rimossa)

--- modules/databricks/outputs.tf
+++ modules/databricks/outputs.tf
@@ -9,7 +9,3 @@ output "name" {
 output "workspace_url" {
   value = azurerm_databricks_workspace.this.workspace_url
 }
-
-output "principal_id" {
-  value = azurerm_databricks_workspace.this.identity[0].principal_id
-}
